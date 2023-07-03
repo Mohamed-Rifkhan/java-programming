9c918882124c3aaffafc333fc8350c69ad56368f
Mohamed-Rifkhan <138417182+Mohamed-Rifkhan@users.noreply.github.com>
2023-07-03 15:55:14 +0700

    Add files via upload

---
+++ chapter1/Exercise1_4.java
@@ -0,0 +1,6 @@
+public class Exercise1_4 {
+    public static void main(String[] args) {
+        System.out.println((312032486)+((5 * 365 * 24 * 60 * 60)/7)-((5 * 365 * 24 * 60 * 60)/13)+((5 * 365 * 24 * 60 * 60)/45));  
+
+    }
+}
